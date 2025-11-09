079dbb3e7c87031b5ecaa4b6e942faf8f37763b6
shenlu89 <shenlu89dev@gmail.com>
2025-11-09 15:39:41 +0800

    refactor(layout): consolidate dashboard layout into root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,31 @@
 import "./globals.css";
 import { type ReactNode, Suspense } from "react";
-import { Cantarell } from "@/lib/fonts";
+import { Outfit, Roboto_Mono } from "next/font/google";
+const outfit = Outfit({
+  variable: "--font-outfit",
+  subsets: ["latin"],
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+import Header from "@/components/header";
+import { Footer } from "@/components/footer";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body
-        className={`flex flex-col bg-slate-50 h-screen ${Cantarell.className}`}
+        className={`${outfit.variable} ${robotoMono.variable} font-sans antialiased h-screen flex flex-col bg-gray-50`}
         suppressHydrationWarning
       >
-        <Suspense fallback={null}>{children}</Suspense>
+        <Header />
+        <main className="flex relative flex-col items-center flex-1 m-auto w-full max-w-6xl p-4">
+          <Suspense fallback={null}>{children}</Suspense>
+        </main>
+        <Footer />
       </body>
     </html>
   );
